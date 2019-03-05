4c6fdfc905fa38dca779c1f31b7570417e23a3d2
Matte <mattias4@kth.se>
2019-03-05 20:03:46 +0100

    Added support for local token overrides

--- grab.py
+++ grab.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python3
 import argparse
+import json
 import re
 import sys
+from pathlib import Path
 
 from signal import signal, SIGPIPE, SIG_DFL
 
 signal(SIGPIPE, SIG_DFL)
 
-PATTERNS = {
+TOKENS = {
     'd': r'[0-9]+',
     'i': r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}',
     'a': r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:["
@@ -20,27 +22,46 @@ PATTERNS = {
          r'3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:['
          r'\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])',
     'q': r'"(?:[^"\\]|\\.)*"',
+    'Q': r"'(?:[^'\\]|\\.)*'",
     'w': r'\S+',
     '[': r'\[[^\]]*\]'
 }
 
-parser = argparse.ArgumentParser(description='grabs tokens from lines of text.')
+parser = argparse.ArgumentParser(description='grabs tokens from stdin (e.g. `grab qdd 13 <README`)',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('command',
                     action='store',
                     help='specifies what tokens to grab')
 parser.add_argument('tokens',
                     nargs='?',
                     help='Which tokens to output (1-based, defaults to all)')
-parser.epilog = ("token types: d=integer number, i=IP address, a=IP or domain name, e=email address, q=double quoted "
-                 "string, w=word, [=square bracketed text ")
+parser.epilog = """
+token types:
+    d: integer number
+    i: IP address
+    a: IP or domain name
+    e: email address
+    q: single quoted string
+    Q: double quoted string
+    w: word
+    [: square bracketed text
+
+override or add token types:
+    Add a json object with token name (single char) and corresponding regex to ~/.grab/tokens.json
+"""
 args = parser.parse_args()
+
+additional_patterns = Path.home().joinpath('.grab').joinpath('tokens.json')
+if additional_patterns.is_file():
+    with open(str(additional_patterns)) as f:
+        TOKENS = {**TOKENS, **json.load(f)}
 
 indices = [int(c) for c in args.tokens or range(1, len(args.command) + 1)]
 
 for line in sys.stdin:
     tokens = []
     for c in args.command:
-        pattern = PATTERNS[c]
+        pattern = TOKENS[c]
         match = re.search(pattern, line)
         if match:
             begin, end = match.span()
