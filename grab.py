90629f0a7a0f5c013d7d933eeabe5f43db558531
Matte <mattias4@kth.se>
2020-08-10 18:17:04 +0200

    Extracted main code to a function

--- grab.py
+++ grab.py
@@ -9,7 +9,7 @@ from signal import signal, SIGPIPE, SIG_DFL
 
 signal(SIGPIPE, SIG_DFL)
 
-TOKENS = {
+PATTERNS = {
     'd': r'[0-9]+',
     'i': r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}',
     'a': r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:["
@@ -21,60 +21,77 @@ TOKENS = {
          r'a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){'
          r'3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:['
          r'\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])',
-    'q': r'"(?:[^"\\]|\\.)*"',
-    'Q': r"'(?:[^'\\]|\\.)*'",
+    'q': r"'(?:[^'\\]|\\.)*'",
+    'Q': r'"(?:[^"\\]|\\.)*"',
     'w': r'\S+',
-    '[': r'\[[^\]]*\]'
+    '[': r'\[[^\]]*\]',
 }
 
-parser = argparse.ArgumentParser(description='grabs tokens from stdin (e.g. `grab qdd 13 <README`)',
-                                 formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('command',
-                    action='store',
-                    help='specifies what tokens to grab')
-parser.add_argument('tokens',
-                    nargs='?',
-                    help='Which tokens to output (1-based, defaults to all)')
-parser.epilog = """
-token types:
-    d: integer number
-    i: IP address
-    a: IP or domain name
-    e: email address
-    q: single quoted string
-    Q: double quoted string
-    w: word
-    [: square bracketed text
+PATTERNS_HELP = {
+    'd': 'integer',
+    'i': 'IPv4 address',
+    'a': 'Address (hostname or IP',
+    'e': 'Email address',
+    'q': "'-quoted string",
+    'Q': '"-quoted string',
+    'w': r'"word" (\S+)',
+    '[': 'Square bracketed text'
+}
 
-override or add token types:
-    Add a json object with token name (single char) and corresponding regex to ~/.grab/tokens.json
-"""
-args = parser.parse_args()
 
-additional_patterns = Path.home().joinpath('.grab').joinpath('tokens.json')
-if additional_patterns.is_file():
-    with open(str(additional_patterns)) as f:
-        TOKENS = {**TOKENS, **json.load(f)}
+def list_of_ints(digits):
+    return [int(digit) for digit in digits]
 
-indices = [int(c) for c in args.tokens or range(1, len(args.command) + 1)]
 
-for line in sys.stdin:
+def grab_tokens(line, command, patterns, projection):
     tokens = []
-    for c in args.command:
-        pattern = TOKENS[c]
+    for c in command:
+        pattern = patterns[c]
         match = re.search(pattern, line)
         if match:
             begin, end = match.span()
             tokens.append(line[begin: end])
             line = line[end:]
 
-    output = []
-    for i in indices:
-        try:
-            output.append(tokens[i - 1])
-        except IndexError:
-            pass
+    if projection:
+        tokens = [tokens[i] for i in projection if i < len(tokens)]
 
-    output = '\t'.join(output)
-    if output:
-        print(output, flush=True)
+    return tokens
+
+
+def main():
+    patterns = PATTERNS
+    additional_patterns = Path.home().joinpath('.grabrc')
+    if additional_patterns.is_file():
+        patterns = {**PATTERNS, **json.load(additional_patterns.open())}
+
+    parser = argparse.ArgumentParser(
+        description='grabs and prints specified tokens from lines in stdin (e.g. `grab qdd 13 <README`)',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        'command',
+        action='store',
+        help='specifies what tokens to grab as a string of single-character token names',
+    )
+    parser.add_argument(
+        'projection',
+        nargs='?',
+        help='projection of grabbed tokens to output as a string of indices (0 through 9), defaults to all tokens',
+        type=list_of_ints
+    )
+    parser.epilog = 'Known token types:\n'
+    for pattern in patterns:
+        parser.epilog += f'  {pattern}: {PATTERNS_HELP.get(pattern, "")}\n'
+    parser.epilog += '\nOverride or add new tokens with ~/.grabrc (JSON: { "a": "someregex" }'
+    args = parser.parse_args()
+
+    for line in sys.stdin:
+        tokens = grab_tokens(line, args.command, patterns, args.projection)
+
+        if tokens:
+            print(*tokens, sep='\t', flush=True)
+
+
+if __name__ == '__main__':
+    main()
