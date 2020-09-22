05cf12b8117fadb7266e5dae068d4c942ce01a9b
Matte <mattias4@kth.se>
2020-09-22 21:15:04 +0200

    Added Obsoletes-Dist metadata

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='folkol.grab',
-    version='0.0.4',
+    version='0.0.5',
     description='a unix filter for extracting tokens',
     long_description=long_description,
     long_description_content_type='text/markdown',
